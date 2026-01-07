d5160009730aee683b976abc260f275b5df11dd2
vlcdck <pridatchenkovladislav@gmail.com>
2026-01-07 23:51:00 +0200

    Init

---
+++ src/main/java/com/vendo/aws_servcie/AwsServcieApplication.java
@@ -0,0 +1,13 @@
+package com.vendo.aws_servcie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsServcieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsServcieApplication.class, args);
+    }
+
+}
