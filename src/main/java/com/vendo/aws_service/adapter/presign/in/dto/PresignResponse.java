bd2321e5498991785bad7f5697e1b81a53392f0f
HaaaamBurger <gemasclashes@gmail.com>
2026-07-18 15:26:17 +0300

    kan-85: mapping data types for response

--- src/main/java/com/vendo/aws_service/adapter/presign/in/dto/PresignResponse.java
+++ src/main/java/com/vendo/aws_service/adapter/presign/in/dto/PresignResponse.java
@@ -5,7 +5,7 @@ import com.vendo.aws_service.domain.presign.dto.PresignBody;
 import java.util.List;
 
 public record PresignResponse(
-        List<PresignBody> files
+        List<PresignBody> data
 ) {
 
     public static PresignResponse of(List<PresignBody> files) {
