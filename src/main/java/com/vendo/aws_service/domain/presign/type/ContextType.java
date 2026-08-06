01b23301f01aa96e43726338121b98062da8ab97
HaaaamBurger <gemasclashes@gmail.com>
2026-08-06 10:48:43 +0300

    kan-104: category context type

--- src/main/java/com/vendo/aws_service/domain/presign/type/ContextType.java
+++ src/main/java/com/vendo/aws_service/domain/presign/type/ContextType.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ContextType {
 
-    PRODUCT("products");
+    PRODUCT("products"),
+    CATEGORY("categories");
 
     private final String path;
 }
